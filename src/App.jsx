fadeaeddcb05aa92ee94a046f74c58b3ce21aae6
Nasradin <nesreddin19@gmail.com>
2024-08-09 10:11:07 +0400

    Nasradin Guseinovi _

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,4 @@
-
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import axios from "axios";
-
-
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Context from './context/context'; 
 import Homepage from './components/Homepage';
@@ -16,34 +11,30 @@ import About from './pages/About';
 import Apply from './pages/Apply';
 import AddPost from './pages/AddPost';
 
+
 function App() {
   const data = {}; 
 
   return (
     <div className="app-container">
-        <Context.Provider value={data}>
-          <div className="navbar-component">
-            <Navbar />
-          </div>
-          <div className="all-components">
-            <Routes>
-              <Route path='/' element={<Homepage />} />
-              <Route path='/details/:id' element={<Details/>}/>
-              <Route path='/service' element={<Service/>}/>
-              <Route path='/contact' element={<Contact/>}/>
-              <Route path='/about' element={<About/>}/>
-              <Route path='/apply' element={<Apply/>}/>
-              <Route path='/add-post' element={<AddPost/>} />
-            </Routes>
-          </div>
-        </Context.Provider>
+      <Context.Provider value={data}>
+        <div className="navbar-component">
+          <Navbar />
+        </div>
+        <div className="all-components">
+          <Routes>
+            <Route path='/' element={<Homepage />} />
+            <Route path='/details/:id' element={<Details/>}/>
+            <Route path='/service' element={<Service/>}/>
+            <Route path='/contact' element={<Contact/>}/>
+            <Route path='/about' element={<About/>}/>
+            <Route path='/apply' element={<Apply/>}/>
+            <Route path='/add-post' element={<AddPost/>} />
+          </Routes>
+        </div>
+      </Context.Provider>
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
-import '../css/Navbar.css'
+import React from "react";
+import { IoMdSunny } from "react-icons/io";
+import { FaMoon } from "react-icons/fa";
+import "../css/Navbar.css";
 
 const Navbar = () => {
   return (
     <>
+      <div className="sidebar">
+        <div className="logo">
+          <img src="src/img/logo.c9da023 (1).png" alt="logo" />
+        </div>
+        <div>
+          <h2>Elanlar</h2>
+          <h2>Elan yerləşdir</h2>
+        </div>
+        <div className="icon-container">
+          <div className="sun">
+            <IoMdSunny size={15} />
+          </div>
+          <div className="moon">
+            <FaMoon size={15} />
+          </div>
+        </div>
+        <select name="lang" id="lang">
+          <option value="Az">Az</option>
+          <option value="En">En</option>
+          <option value="Ru">Ru</option>
+        </select>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
