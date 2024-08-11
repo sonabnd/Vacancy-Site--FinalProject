b0e6e9acd7695aa16495a345d6937e489a6cabee
Qadir <qadir.rustamov2006@gmail.com>
2024-08-12 00:24:34 +0400

    rustamovv qadir

--- src/App.jsx
+++ src/App.jsx
@@ -1,172 +1,6 @@
 
 
-// import React, { useState } from 'react';
-// import { Route, Routes } from 'react-router-dom';
-// import Context from './context/context';
-// import Homepage from './components/Homepage';
-// import Navbar from './components/Navbar';
-// import './App.css';
-// import Details from './pages/Details';
-// import Service from './pages/Service';
-// import Contact from './pages/Contact';
-// import About from './pages/About';
-// import Apply from './pages/Apply';
-// import AddPost from './pages/AddPost';
-// import { IoClose } from 'react-icons/io5';
-// import { RiLoginCircleLine } from 'react-icons/ri';
-
-// function App() {
-//   // login/register
-//   const [login, setLogin] = useState(false)
-//   const [register, setRegister] = useState(false)
-//   const [isDesign, setDesign] = useState(false)
-
-//   const handleShowLogin = () => {
-//     setLogin(true)
-//     setRegister(false)
-//     setDesign(true)
-//   }
-
-//   const handleCloseLogin = () => {
-//     setLogin(false)
-//     setDesign(false)
-//   }
-
-//   const handleShowRegister = () => {
-//     setRegister(true)
-//     setLogin(false)
-//     setDesign(true)
-//   }
-
-//   const handleCloseRegister = () => {
-//     setRegister(false)
-//     setDesign(false)
-//   }
-
-
-//   const data = {
-//     login,
-//     setLogin
-//   };
-
-//   return (
-//     <>
-//       <div className={isDesign ? "open" : 'close'}>
-//         <button onClick={handleShowLogin}>
-//           Elan yerlesdir
-//         </button>
-//         <div className="modal-content-login">
-//           {
-//             login && (
-//               <div className="login">
-//                 <div className="login-main">
-//                   <div onClick={handleCloseLogin} className="login-close">
-//                     <IoClose />
-//                   </div>
-//                   <div className="login-icon">
-//                     <RiLoginCircleLine />
-//                   </div>
-//                   <div style={{ marginBottom: "10px" }}>
-//                     <h3>Hesabınıza daxil olun</h3>
-//                   </div>
-//                   <div style={{ marginBottom: "10px" }}>
-//                     <p>Xoş gəlmişsiniz!</p>
-//                   </div>
-//                   <div className="login-inputs">
-//                     <div className="login-input">
-//                       <label htmlFor="">E-poçt</label>
-//                       <input type="text" placeholder="E-poçtunuzu yaradın" />
-//                     </div>
-//                     <div className="login-input">
-//                       <label htmlFor="">Şifrə</label>
-//                       <input type="password" placeholder="Şifrə" />
-//                     </div>
-//                   </div>
-//                   <div className="login-button">
-//                     <button>Daxil ol</button>
-//                   </div>
-//                   <div className="qeydiyyat-button">
-//                     <button onClick={handleShowRegister}>Qeydiyyatdan keç</button>
-//                   </div>
-//                 </div>
-//               </div>
-//             )
-//           }
-//           {
-//             register && (
-//               <div className="register">
-//                 <div className="register-main">
-//                   <div onClick={handleCloseRegister} className="register-close">
-//                     <IoClose />
-//                   </div>
-//                   <div className="register-head">
-//                     <div className="register-icon">
-//                       <img loading="lazy" src="https://www.hellojob.az/content/assets/images/login/flag.svg" alt="" />
-//                     </div>
-//                     <div className="register-head-text">
-//                       <h3>Qeydiyyatdan keçin</h3>
-//                     </div>
-//                   </div>
-//                   <div className="regisetr-inputs">
-//                     <div className="register-input">
-//                       <label htmlFor="">Adınız*</label>
-//                       <input type="text" placeholder="Adinizi daxil edin" />
-//                     </div>
-//                     <div className="register-input">
-//                       <label htmlFor="">E-poçt*</label>
-//                       <input type="text" placeholder="E-poçtunuzu yazın" />
-//                     </div>
-//                     <div className="register-input">
-//                       <label htmlFor="">Şifre*</label>
-//                       <input type="password" />
-//                     </div>
-//                     <div className="register-input">
-//                       <label htmlFor="">Şifrə təkrarı*</label>
-//                       <input type="password" />
-//                     </div>
-//                   </div>
-//                   <div className="register-button">
-//                     <button>Qeydiyyat ol</button>
-//                   </div>
-//                   <div className="register-button-giris">
-//                     <button onClick={handleShowLogin}>Giriş sehifəsi</button>
-//                   </div>
-//                 </div>
-//               </div>
-//             )
-//           }
-//         </div>
-//         <div className="app-container">
-//           <Context.Provider value={data}>
-//             <div className="navbar-component">
-//               <Navbar />
-//             </div>
-//             <div className="all-components">
-//               <Routes>
-//                 <Route path='/' element={<Homepage />} />
-//                 <Route path='/details/:id' element={<Details />} />
-//                 <Route path='/service' element={<Service />} />
-//                 <Route path='/contact' element={<Contact />} />
-//                 <Route path='/about' element={<About />} />
-//                 <Route path='/apply' element={<Apply />} />
-//                 <Route path='/add-post' element={<AddPost />} />
-//               </Routes>
-//             </div>
-//           </Context.Provider>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-
-
-
-
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Context from './context/context';
 import Homepage from './components/Homepage';
@@ -178,30 +12,197 @@ import Contact from './pages/Contact';
 import About from './pages/About';
 import Apply from './pages/Apply';
 import AddPost from './pages/AddPost';
+import { IoClose } from 'react-icons/io5';
+import { RiLoginCircleLine } from 'react-icons/ri';
 
 function App() {
-  const data = {}; // Context data, if needed
+  // login/register
+  const [login, setLogin] = useState(false)
+  const [register, setRegister] = useState(false)
+  const [isDesign, setDesign] = useState(false)
+
+  const handleShowLogin = () => {
+    setLogin(true)
+    setRegister(false)
+    setDesign(true)
+  }
+
+  const handleCloseLogin = () => {
+    setLogin(false)
+    setDesign(false)
+  }
+
+  const handleShowRegister = () => {
+    setRegister(true)
+    setLogin(false)
+    setDesign(true)
+  }
+
+  const handleCloseRegister = () => {
+    setRegister(false)
+    setDesign(false)
+  }
+
+
+  const data = {
+    login,
+    setLogin,
+    register,
+    setRegister,
+    isDesign,
+    setDesign
+  };
 
   return (
-    <div className="app-container">
-      <Context.Provider value={data}>
-        <div className="navbar-component">
-          <Navbar />
+    <>
+      <div className={isDesign ? "open" : 'close'}>
+        <div className="modal-content-login">
+          {
+            login && (
+              <div className="login">
+                <div className="login-main">
+                  <div onClick={handleCloseLogin} className="login-close">
+                    <IoClose />
+                  </div>
+                  <div className="login-icon">
+                    <RiLoginCircleLine />
+                  </div>
+                  <div style={{ marginBottom: "10px" }}>
+                    <h3>Hesabınıza daxil olun</h3>
+                  </div>
+                  <div style={{ marginBottom: "10px" }}>
+                    <p>Xoş gəlmişsiniz!</p>
+                  </div>
+                  <div className="login-inputs">
+                    <div className="login-input">
+                      <label htmlFor="">E-poçt</label>
+                      <input type="text" placeholder="E-poçtunuzu yaradın" />
+                    </div>
+                    <div className="login-input">
+                      <label htmlFor="">Şifrə</label>
+                      <input type="password" placeholder="Şifrə" />
+                    </div>
+                  </div>
+                  <div className="login-button">
+                    <button>Daxil ol</button>
+                  </div>
+                  <div className="qeydiyyat-button">
+                    <button onClick={handleShowRegister}>Qeydiyyatdan keç</button>
+                  </div>
+                </div>
+              </div>
+            )
+          }
+          {
+            register && (
+              <div className="register">
+                <div className="register-main">
+                  <div onClick={handleCloseRegister} className="register-close">
+                    <IoClose />
+                  </div>
+                  <div className="register-head">
+                    <div className="register-icon">
+                      <img loading="lazy" src="https://www.hellojob.az/content/assets/images/login/flag.svg" alt="" />
+                    </div>
+                    <div className="register-head-text">
+                      <h3>Qeydiyyatdan keçin</h3>
+                    </div>
+                  </div>
+                  <div className="regisetr-inputs">
+                    <div className="register-input">
+                      <label htmlFor="">Adınız*</label>
+                      <input type="text" placeholder="Adinizi daxil edin" />
+                    </div>
+                    <div className="register-input">
+                      <label htmlFor="">E-poçt*</label>
+                      <input type="text" placeholder="E-poçtunuzu yazın" />
+                    </div>
+                    <div className="register-input">
+                      <label htmlFor="">Şifre*</label>
+                      <input type="password" />
+                    </div>
+                    <div className="register-input">
+                      <label htmlFor="">Şifrə təkrarı*</label>
+                      <input type="password" />
+                    </div>
+                  </div>
+                  <div className="register-button">
+                    <button>Qeydiyyat ol</button>
+                  </div>
+                  <div className="register-button-giris">
+                    <button onClick={handleShowLogin}>Giriş sehifəsi</button>
+                  </div>
+                </div>
+              </div>
+            )
+          }
         </div>
-        <div className="all-components">
-          <Routes>
-            <Route path='/' element={<Homepage />} />
-            <Route path='/details/:id' element={<Details />} />
-            <Route path='/service' element={<Service />} />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/apply' element={<Apply />} />
-            <Route path='/add-post' element={<AddPost />} />
-          </Routes>
+        <div className="app-container">
+          <Context.Provider value={data}>
+            <div className="navbar-component">
+              <Navbar />
+            </div>
+            <div className="all-components">
+              <Routes>
+                <Route path='/' element={<Homepage />} />
+                <Route path='/details/:id' element={<Details />} />
+                <Route path='/service' element={<Service />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/apply' element={<Apply />} />
+                <Route path='/add-post' element={<AddPost />} />
+              </Routes>
+            </div>
+          </Context.Provider>
         </div>
-      </Context.Provider>
-    </div>
+      </div>
+    </>
   );
 }
 
 export default App;
+
+
+
+
+
+
+// import React from 'react';
+// import { Route, Routes } from 'react-router-dom';
+// import Context from './context/context';
+// import Homepage from './components/Homepage';
+// import Navbar from './components/Navbar';
+// import './App.css';
+// import Details from './pages/Details';
+// import Service from './pages/Service';
+// import Contact from './pages/Contact';
+// import About from './pages/About';
+// import Apply from './pages/Apply';
+// import AddPost from './pages/AddPost';
+
+// function App() {
+//   const data = {}; // Context data, if needed
+
+//   return (
+//     <div className="app-container">
+//       <Context.Provider value={data}>
+//         <div className="navbar-component">
+//           <Navbar />
+//         </div>
+//         <div className="all-components">
+//           <Routes>
+//             <Route path='/' element={<Homepage />} />
+//             <Route path='/details/:id' element={<Details />} />
+//             <Route path='/service' element={<Service />} />
+//             <Route path='/contact' element={<Contact />} />
+//             <Route path='/about' element={<About />} />
+//             <Route path='/apply' element={<Apply />} />
+//             <Route path='/add-post' element={<AddPost />} />
+//           </Routes>
+//         </div>
+//       </Context.Provider>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -207,19 +207,16 @@
 
 
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { IoMdSunny } from "react-icons/io";
 import { FaMoon } from "react-icons/fa";
-import { IoClose } from 'react-icons/io5';
-import { RiLoginCircleLine } from 'react-icons/ri';
 import '../css/Navbar.css';
 import '../css/NavbarResponsive.css';
+import Context from '../context/context';
 
 const Navbar = () => {
   const [isDarkMode, setIsDarkMode] = useState(false);
-  const [login, setLogin] = useState(false);
-  const [register, setRegister] = useState(false);
-  const [isDesign, setDesign] = useState(false);
+  const { setLogin, setRegister, setDesign } = useContext(Context)
 
   useEffect(() => {
     if (isDarkMode) {
@@ -239,21 +236,6 @@ const Navbar = () => {
     setDesign(true);
   };
 
-  const handleCloseLogin = () => {
-    setLogin(false);
-    setDesign(false);
-  };
-
-  const handleShowRegister = () => {
-    setRegister(true);
-    setLogin(false);
-    setDesign(true);
-  };
-
-  const handleCloseRegister = () => {
-    setRegister(false);
-    setDesign(false);
-  };
 
   return (
     <div className="sidebar">
@@ -261,14 +243,14 @@ const Navbar = () => {
         <div className="header_logo">
           <img src="/src/img/logo.c9da023 (1).png" alt="logo" />
         </div>
-        <select name="lang" id="lang" aria-label="Language Selector">
-          <option value="Az">Az</option>
-          <option value="En">En</option>
-          <option value="Ru">Ru</option>
+        <select className='select-lang' name="lang" id="lang" aria-label="Language Selector">
+          <option className='select-lang-option' value="Az">Az</option>
+          <option className='select-lang-option' value="En">En</option>
+          <option className='select-lang-option' value="Ru">Ru</option>
         </select>
         <ul className="header__menu">
           <li className="header__menu__item1">Elanlar</li>
-          <li className="header__menu__item2" onClick={handleShowLogin}>Elan yerləşdir</li>
+          <li className="header__menu__item1" onClick={handleShowLogin}>Elan yerləşdir</li>
         </ul>
         <div className="icon-container" onClick={handleToggleDarkMode}>
           <div className="sun">
@@ -287,86 +269,6 @@ const Navbar = () => {
           <li className="footer_menu_item3">Əlaqə</li>
         </ul>
       </footer>
-
-      <div className={isDesign ? "open" : 'close'}>
-        <div className="modal-content-login">
-          {login && (
-            <div className="login">
-              <div className="login-main">
-                <div onClick={handleCloseLogin} className="login-close">
-                  <IoClose />
-                </div>
-                <div className="login-icon">
-                  <RiLoginCircleLine />
-                </div>
-                <div style={{ marginBottom: "10px" }}>
-                  <h3>Hesabınıza daxil olun</h3>
-                </div>
-                <div style={{ marginBottom: "10px" }}>
-                  <p>Xoş gəlmişsiniz!</p>
-                </div>
-                <div className="login-inputs">
-                  <div className="login-input">
-                    <label htmlFor="">E-poçt</label>
-                    <input type="text" placeholder="E-poçtunuzu yaradın" />
-                  </div>
-                  <div className="login-input">
-                    <label htmlFor="">Şifrə</label>
-                    <input type="password" placeholder="Şifrə" />
-                  </div>
-                </div>
-                <div className="login-button">
-                  <button>Daxil ol</button>
-                </div>
-                <div className="qeydiyyat-button">
-                  <button onClick={handleShowRegister}>Qeydiyyatdan keç</button>
-                </div>
-              </div>
-            </div>
-          )}
-          {register && (
-            <div className="register">
-              <div className="register-main">
-                <div onClick={handleCloseRegister} className="register-close">
-                  <IoClose />
-                </div>
-                <div className="register-head">
-                  <div className="register-icon">
-                    <img loading="lazy" src="https://www.hellojob.az/content/assets/images/login/flag.svg" alt="" />
-                  </div>
-                  <div className="register-head-text">
-                    <h3>Qeydiyyatdan keçin</h3>
-                  </div>
-                </div>
-                <div className="regisetr-inputs">
-                  <div className="register-input">
-                    <label htmlFor="">Adınız*</label>
-                    <input type="text" placeholder="Adinizi daxil edin" />
-                  </div>
-                  <div className="register-input">
-                    <label htmlFor="">E-poçt*</label>
-                    <input type="text" placeholder="E-poçtunuzu yazın" />
-                  </div>
-                  <div className="register-input">
-                    <label htmlFor="">Şifre*</label>
-                    <input type="password" />
-                  </div>
-                  <div className="register-input">
-                    <label htmlFor="">Şifrə təkrarı*</label>
-                    <input type="password" />
-                  </div>
-                </div>
-                <div className="register-button">
-                  <button>Qeydiyyat ol</button>
-                </div>
-                <div className="register-button-giris">
-                  <button onClick={handleShowLogin}>Giriş sehifəsi</button>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-      </div>
     </div>
   );
 };
