c1d92ca57a54f29796d6a1bcfba19fa7a8e54ad0
Qadir <qadir.rustamov2006@gmail.com>
2024-08-12 00:26:17 +0400

    rustamovv qadir

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-
-
 import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Context from './context/context';
@@ -163,46 +161,3 @@ function App() {
 export default App;
 
 
-
-
-
-
-// import React from 'react';
-// import { Route, Routes } from 'react-router-dom';
-// import Context from './context/context';
-// import Homepage from './components/Homepage';
-// import Navbar from './components/Navbar';
-// import './App.css';
-// import Details from './pages/Details';
-// import Service from './pages/Service';
-// import Contact from './pages/Contact';
-// import About from './pages/About';
-// import Apply from './pages/Apply';
-// import AddPost from './pages/AddPost';
-
-// function App() {
-//   const data = {}; // Context data, if needed
-
-//   return (
-//     <div className="app-container">
-//       <Context.Provider value={data}>
-//         <div className="navbar-component">
-//           <Navbar />
-//         </div>
-//         <div className="all-components">
-//           <Routes>
-//             <Route path='/' element={<Homepage />} />
-//             <Route path='/details/:id' element={<Details />} />
-//             <Route path='/service' element={<Service />} />
-//             <Route path='/contact' element={<Contact />} />
-//             <Route path='/about' element={<About />} />
-//             <Route path='/apply' element={<Apply />} />
-//             <Route path='/add-post' element={<AddPost />} />
-//           </Routes>
-//         </div>
-//       </Context.Provider>
-//     </div>
-//   );
-// }
-
-// export default App;
